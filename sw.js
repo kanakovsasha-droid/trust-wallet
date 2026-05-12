7a9eee66bd518b55cbfd700bb0110127d906cb9c
Alex <test@test.com>
2026-05-13 01:50:41 +0300

    Update to new Figma design v3
    - Add new uploads (photos, drawings, pasted images) from v3
    - Add screenshot.jpg
    - Add scrap references v3, v5, v5-desktop
    - Bump SW cache to wallet-v3 with all new assets
    - Manifest: restore scope and add 192px icon size
    - Register service worker in index.html

--- sw.js
+++ sw.js
@@ -1,12 +1,30 @@
-const CACHE = 'wallet-v2';
+const CACHE = 'wallet-v3';
 const ASSETS = [
   './',
   './index.html',
   './manifest.webmanifest',
   './icon.png',
+  './screenshot.jpg',
   './uploads/ui.jpg',
+  './uploads/IMG_9273.PNG',
+  './uploads/IMG_9273-5e159e68.PNG',
+  './uploads/IMG_9385.JPEG',
+  './uploads/draw-02dd4e23-4bac-424f-81ac-05af64ecb670.png',
+  './uploads/draw-ecdcc609-7d3c-4e47-b461-e5d7d19c960a.png',
+  './uploads/pasted-1778598338422-0.png',
+  './uploads/pasted-1778624104671-0.png',
+  './uploads/pasted-1778624191276-0.png',
+  './uploads/pasted-1778624633522-0.png',
+  './uploads/pasted-1778624848336-0.png',
+  './uploads/pasted-1778625112938-0.png',
+  './uploads/photo_1_2026-05-13_01-07-32.jpg',
+  './uploads/photo_1_2026-05-13_01-07-32-b25fd42e.jpg',
+  './uploads/photo_2_2026-05-13_01-07-32.jpg',
   './scrap/current.png',
-  './scrap/v2.png'
+  './scrap/v2.png',
+  './scrap/v3.png',
+  './scrap/v5.png',
+  './scrap/v5-desktop.png'
 ];
 
 self.addEventListener('install', e => {
